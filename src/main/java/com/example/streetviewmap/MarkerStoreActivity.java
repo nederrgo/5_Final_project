ef7fb150f9d2efa487cfa284bbb6266e7e6e8160
nederrotemgaloz <neder@rgoarchitects.com>
2022-04-04 10:44:08 +0300

    initial commit

--- src/main/java/com/example/streetviewmap/MarkerStoreActivity.java
+++ src/main/java/com/example/streetviewmap/MarkerStoreActivity.java
@@ -1,12 +1,12 @@
 package com.example.streetviewmap;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import java.util.ArrayList;
+
 
 public class MarkerStoreActivity extends BaseActivity {
     ArrayList<RecyclerViewMarkerData> recyclerViewMarkerData;
@@ -15,8 +15,8 @@ public class MarkerStoreActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marker_store);
         RecyclerView markersOptionsToBuy = (RecyclerView) findViewById(R.id.recyclerview);
-        String[] markersNames={"doge marker","doge marker"};
-        int[] markersId={R.drawable.doge_marker,R.drawable.doge_marker};
+        String[] markersNames={"doge marker","fireBase marker"};
+        int[] markersId={R.drawable.doge_marker,R.drawable.fire_base_marker};
         recyclerViewMarkerData=RecyclerViewMarkerData.createMarkersList(markersNames,markersId);
         MarkersAdpters markersAdpters= new MarkersAdpters(recyclerViewMarkerData);
         markersOptionsToBuy.setAdapter(markersAdpters);
